9ec49209132956407ab536df23e90c14e7e381cb
bykovaaa <bykovaaa@silaunion.ru>
2024-03-17 17:23:05 +0300

    + delete test data method for UI test

--- src/main/java/com/example/teamcity/api/generators/TestDataGenerator.java
+++ src/main/java/com/example/teamcity/api/generators/TestDataGenerator.java
@@ -92,4 +92,58 @@ public class TestDataGenerator {
                 .buildType(buildTypeWithEmptyData)
                 .build();
     }
+
+    public static String convertNameToId(String name) {
+        String[] parts = name.split("_");
+        StringBuilder idBuilder = new StringBuilder("");
+
+        idBuilder.append(parts[0].substring(0, 1).toUpperCase()).append(parts[0].substring(1));
+
+        for (int i = 1; i < parts.length; i++) {
+            idBuilder.append(parts[i].substring(0, 1).toUpperCase()).append(parts[i].substring(1));
+        }
+
+        return idBuilder.toString();
+    }
+
+    public static TestData generateDataForUITest() {
+
+        var user = User.builder()
+                .username(RandomData.getString())
+                .password(RandomData.getString())
+                .email(RandomData.getString() + "@gmail.com")
+                .roles(Roles.builder()
+                        .role(Arrays.asList(Role.builder()
+                                .roleId("SYSTEM_ADMIN")
+                                .scope("g")
+                                .build()))
+                        .build())
+                .build();
+
+        var project = NewProjectDescription
+                .builder()
+                .parentProject(Project.builder()
+                        .locator("_Root")
+                        .build())
+                .name(RandomData.getString())
+                .id(RandomData.getString())
+                .copyAllAssociatedSettings(true)
+                .build();
+
+        // Преобразование имени проекта в идентификатор
+        String projectId = convertNameToId(project.getName());
+        project.setId(projectId);
+
+        var buildType = BuildType.builder()
+                .id(RandomData.getString())
+                .name(RandomData.getString())
+                .project(project)
+                .build();
+
+        return TestData.builder()
+                .user(user)
+                .project(project)
+                .buildType(buildType)
+                .build();
+    }
 }
